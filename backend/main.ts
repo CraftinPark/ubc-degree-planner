e54988f5de2653a1d0d827e7e7ece8c82336b8e2
CraftinPark <craftinpark@gmail.com>
2022-06-23 17:52:57 -0700

    setup main.ts

--- backend/main.ts
+++ backend/main.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+const fs = require("fs");
 
 dotenv.config();
 
@@ -9,5 +10,25 @@ const port = process.env.PORT;
 app.get("/", (req: Request, res: Response) => {});
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
+
+const MOCK_LIST = __dirname + "/../mock-list.txt";
+import Course from "./src/Course";
+
+let file = fs.readFileSync(MOCK_LIST, "utf-8");
+let courses = [];
+let course;
+for (let line of file.toString().split("\n")) {
+    let parts = line.split(":");
+    if (parts.length == 1) continue;
+    let attribute = parts[0].trim();
+    let value = parts[1].trim();
+    if (attribute === "code") {
+        course = new Course(value);
+        courses.push(course);
+    } else {
+        course.setAttribute(attribute, value);
+    }
+}
+console.log(courses);
