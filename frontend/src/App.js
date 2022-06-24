bc57633df90fe0df9d3194d0c8d0b6fe359646d9
CraftinPark <craftinpark@gmail.com>
2022-06-23 17:52:23 -0700

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,57 @@
-import { Text } from "@blueprintjs/core"
-
+import { Button, InputGroup } from "@blueprintjs/core";
 
 function App() {
-  return <div className="App"><h1>ubc-degree-planner</h1></div>;
+
+  // make a data request and fill variable 'courses'
+
+  let firstYearCourses = "";
+  let secondYearCourses = "";
+  let thirdYearCourses = "";
+  let fourthYearCourses = "";
+
+  return (
+    <div className="App">
+      <h1>ubc-degree-planner</h1>
+      First Year Courses:{" "}
+      <InputGroup
+        className="first-year-courses"
+        onChange={(e) => {
+          firstYearCourses = e.target.value;
+        }}
+      ></InputGroup>
+      Second Year Courses:{" "}
+      <InputGroup
+        className="first-year-courses"
+        onChange={(e) => {
+          secondYearCourses = e.target.value;
+        }}
+      ></InputGroup>
+      Third Year Courses:{" "}
+      <InputGroup
+        className="first-year-courses"
+        onChange={(e) => {
+          thirdYearCourses = e.target.value;
+        }}
+      ></InputGroup>
+      Fourth Year Courses:{" "}
+      <InputGroup
+        className="first-year-courses"
+        onChange={(e) => {
+          fourthYearCourses = e.target.value;
+        }}
+      ></InputGroup>
+      <Button
+        onClick={() => {
+          console.log(firstYearCourses);
+          console.log(secondYearCourses);
+          console.log(thirdYearCourses);
+          console.log(fourthYearCourses);
+        }}
+      >
+        print courses
+      </Button>
+    </div>
+  );
 }
 
 export default App;
